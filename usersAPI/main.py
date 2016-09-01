92975f07ca91b4ffcc167c5da71d6ec077e53444
Kaustubh Joshi <thatsmekaustubh@gmail.com>
2016-09-01 11:38:17 +0000

    Users API Example

---
+++ usersAPI/main.py
@@ -0,0 +1,36 @@
+from google.appengine.api import users
+import webapp2
+
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        if user:
+            nickname = user.nickname()
+            logout_url = users.create_logout_url('/')
+            greeting = 'Welcome, {}! (<a href="{}">sign out</a>)'.format(
+                nickname, logout_url)
+        else:
+            login_url = users.create_login_url('/')
+            greeting = '<a href="{}">Sign in</a>'.format(login_url)
+
+        self.response.write(
+            '<html><body>{}</body></html>'.format(greeting))
+
+
+class AdminPage(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        if user:
+            if users.is_current_user_admin():
+                self.response.write('You are an administrator.')
+            else:
+                self.response.write('You are not an administrator.')
+        else:
+            self.response.write('You are not logged in.')
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+    ('/admin', AdminPage)
+], debug=True)
